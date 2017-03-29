f055fd439acccdd0dda3d044974ebd98d982695b
Hack Reactor Student <student@hackreactor.com>
2017-03-29 14:57:53 -0700

    Should pass tests for functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var counter = 0;
+  var head = 0;
+  var currentHead = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[head] = value;
+    head++;
+    counter++;
   };
 
   someInstance.dequeue = function() {
+    var val = storage[currentHead];
+    delete storage[currentHead];
+    currentHead++;
+    counter && counter--;
+    return val;
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,25 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var stackSize = 0;
+  
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[stackSize] = value;
+    console.log(value)
+    stackSize++;
   };
 
   someInstance.pop = function() {
+    var val = storage[stackSize -1];
+    delete storage[stackSize];
+    stackSize && stackSize--;
+    console.log(val)
+    return val;
   };
 
   someInstance.size = function() {
+    return stackSize;
   };
 
   return someInstance;
