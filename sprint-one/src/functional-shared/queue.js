1e68cf1aba1b9016fd3a8f8a3dc3537d90cf0881
Hack Reactor Student <student@hackreactor.com>
2017-03-29 15:46:49 -0700

    Should pass the tests for functional shared queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = {};
+  queue.storage = {};
+  queue.count = 0;
+  queue.head = 0;
+  queue.currHead = 0;
+  _.extend(queue, queueMethods);
+  return queue;
 };
 
 var queueMethods = {};
 
+queueMethods.size = function() {
+  return this.count;
+};
 
+queueMethods.enqueue = function(val) {
+  this.storage[this.head] = val;
+  this.head++;
+  this.count++;
+};
+
+queueMethods.dequeue = function() {
+  var item = this.storage[this.currHead];
+  delete this.storage[this.currHead];
+  this.currHead++;
+  this.count && this.count--;
+  return item
+};
