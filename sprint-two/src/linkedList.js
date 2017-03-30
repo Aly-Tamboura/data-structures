ad3ec5fa074996c27407d3acadb35f8423c176a0
Hack Reactor Student <student@hackreactor.com>
2017-03-30 11:40:37 -0700

    Should pass tests on linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,44 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = new Node(value);
+    if(this.head === null && this.tail === null){
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    var oldHead = this.head;
+    var newHead = this.head.next;
+    this.head = newHead;
+    return oldHead.value;
   };
 
   list.contains = function(target) {
+  // var node = this.head;
+  // // console.log(node)
+  // if( node.value === target ) {
+  //   return true;
+  // }
+  // if(node.next) {
+  //   console.log('found more nodes', node.next);
+  //   console.log(contains(target));
+  // }
+
+  var startingNode = this.head;
+
+  while(startingNode !== null){
+    console.log(startingNode);
+    if (startingNode.value === target){
+      return true;
+    }
+    startingNode = startingNode.next;
+  }
+  return false;
   };
 
   return list;
