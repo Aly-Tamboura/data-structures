c6804f830957093ec51bb2e3ade7bb94b3f4263e
Hack Reactor Student <student@hackreactor.com>
2017-03-31 10:33:01 -0700

    Should pass tests for tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,30 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var tree = new Tree(value);
+  this.children.push(tree);
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+  	return true;
+  } else {
+  	for (var i = 0; i < this.children.length; i++) {
+	  var child = this.children[i];
+	  if (child.contains(target)) {
+	  	return true;
+	  }
+  	}
+  }
+  return false;
 };
 
 
