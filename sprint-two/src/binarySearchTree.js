cea3e73c8dcb5899dc43533d96881e2f093d4d72
Hack Reactor Student <student@hackreactor.com>
2017-04-01 11:22:40 -0700

    Ran pomander and fixed syntax

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -14,63 +14,59 @@ BinarySearchTree.prototype.insert = function(value) {
     this.left = tree;
   }
   //check the right side
-  if(this.value < tree.value) {
+  if (this.value < tree.value) {
     //recurse right
-    this.right.insert(tree.value)
+    this.right.insert(tree.value);
   }
 //check the left side
-  if(this.value > tree.value) {
+  if (this.value > tree.value) {
      //recurse left
-     this.left.insert(tree.value)
+    this.left.insert(tree.value);
   }
 };
 
 BinarySearchTree.prototype.contains = function(target) {
   var found = false;
-  // console.log(this.left.value)
-  // console.log(this.right.value)
   var search = function(node) {
-    if(node !== undefined) {
-      if(node && node.value === target ) {
-          found = true;
-        }
-        if(node.right && node.value < target) {
-          node = node.right;
-          search(node)      
-        } else {
-          if(node.left && node.value > target) {
-            node = node.left
-            search(node)
-          }
+    if (node !== undefined) {
+      if (node && node.value === target ) {
+        found = true;
+      }
+      if (node.right && node.value < target) {
+        node = node.right;
+        search(node);      
+      } else {
+        if (node.left && node.value > target) {
+          node = node.left;
+          search(node);
         }
       }
     }
-      search(this);
+  };
+  search(this);
   return found;
 };
 
 BinarySearchTree.prototype.depthFirstLog = function(cb) {
   // body...
   var reachedEnd = false;
-  var traversal = function(node){
-    while (!reachedEnd){
-      if(node.left === null && node.right === null){
+  var traversal = function(node) {
+    while (!reachedEnd) {
+      if (node.left === null && node.right === null) {
         cb(node.value);
         reachedEnd = true;
-      }
-      else if(node.left && node.right === null){
+      } else if (node.left && node.right === null) {
         var leftNode = node.left;
         cb(node.value);
-        traversal(leftNode)
-    }
-       else if(node.right && node.left === null){
+        traversal(leftNode);
+      } else if (node.right && node.left === null) {
         var rightNode = node.right;
         cb(node.value);
         traversal(rightNode);
-     }
-  }
-}
- traversal(this)
+      }
+    }
+  };
+  traversal(this);
 };
 /*
  * Complexity: What is the time complexity of the above functions?
